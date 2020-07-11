9002e41a8067bc1b90ed4ee6832f299cd9405dfc
Usuario <felipe@ubuntu.ubuntu-domain>
2020-07-10 20:07:45 -0400

    inicio + react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,71 @@
-// importar express
-const express = require("express");
-const app = express();
+import React from 'react';
 
-//configuracion del puerto
-app.set("port", process.env.POST || 3000);
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-//Los Middlewares y rutas
-app.use(express.json());
+//import Form from './module/form';
+//import List from './module/list';
+//import Edit from './module/edit';
 
-app.use("/", (req, res) => {
-  res.setHeader("Content-Type", "text/html");
+function App() {
 
-  res.sendFile(__dirname + "/views/index-test.html");
-});
+  return (
+      <div className="App">
+        
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
 
-app.listen(app.get("port"), () => {
-  console.log("Servidor iniciado en http://127.0.0.1:3000/");
-});
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#"> Employee List </a>
+              </li>
+            </ul>
+            <a class="btn btn-info "  href="/form">Add Employee</a>
+          </div>
+        </nav>
+
+        <div class="container py-4">
+          <div class="row">
+
+            // Add component list, form, edit.
+            <table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+          </div>
+        </div>
+
+      </div>
+  );
+}
+
+export default App;
